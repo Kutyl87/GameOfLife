f5371e705630cdff038f14e000bde7539b7973f7
pkutyl <01168894@pw.edu.pl>
2024-05-15 00:09:36 +0200

    Add organ.h file

---
+++ core/include/organ.h
@@ -0,0 +1,15 @@
+
+class Organ {
+public:
+	Organ();
+	Organ(float agingRate, float health) : aging_rate(agingRate), health(health) {}
+	float getAgingRate();
+	float getHealth();
+	float getDamage(float damage);
+	float regenerateHealth(float health);
+	~Organ();
+
+private:
+	float agingRate;
+	float health;
+};
