8bc4773cc6e266fe43d329ac2a0f7f84e1548e5a
pkutyl <01168894@pw.edu.pl>
2024-05-15 02:24:25 +0200

    Add getDamage test

--- tests/organTests.cpp
+++ tests/organTests.cpp
@@ -12,3 +12,9 @@ TEST_CASE("Organ getHealth", "[Organ]") {
 	Organ organ(0.5, 100);
 	REQUIRE(organ.getHealth() == 100);
 }
+
+TEST_CASE("Organ getDamage", "[Organ]") {
+	Organ organ(0.5, 100);
+	organ.getDamage(10);
+	REQUIRE(organ.getHealth() == 90);
+}
