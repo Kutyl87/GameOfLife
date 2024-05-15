2628c3ab9d625ada94c52a8c5146be3ba8c19197
pkutyl <01168894@pw.edu.pl>
2024-05-15 02:21:14 +0200

    Add getHealth Test

--- tests/organTests.cpp
+++ tests/organTests.cpp
@@ -1,7 +1,14 @@
-#include "organ.h"
 #include <catch2/catch_test_macros.hpp>
+#include "organ.h"
 
-TEST_CASE("Organ getAgingRate" "[Organ]"){
+TEST_CASE(
+	"Organ getAgingRate"
+	"[Organ]") {
 	Organ organ(0.5, 100);
 	REQUIRE(organ.getAgingRate() == 0.5);
+}
+
+TEST_CASE("Organ getHealth", "[Organ]") {
+	Organ organ(0.5, 100);
+	REQUIRE(organ.getHealth() == 100);
 }
