3ad3667ceb61ce9c8dde8b709663f385719d7893
Kutyl87 <116507438+Kutyl87@users.noreply.github.com>
2024-05-16 01:01:23 +0200

    20 add class organ (#28)
    * Update .gitignore for .idea editors
    
    * Rename godot folder to godot-project
    
    * Add core folder for plain c++ implementation
    
    * Add folder for implementation in godot
    
    * Add clang-format for project
    
    * Add organ.h file
    
    * Remove unnecessary default constructor
    
    * Add clang-format empty line between blocks
    
    * Add SConstruct
    
    * Add organ impl
    
    * Add gitignore for static lib
    
    * Add scons for basic testing and organTests.cpp
    
    * Add getHealth Test
    
    * Add getDamage test
    
    * Add regenerateHealth test
    
    * Add missing return in scons
    
    * Fix defaultEnvironment Scons
    
    * Rename method to takeDamage

---
+++ core/include/organ.h
@@ -0,0 +1,14 @@
+
+class Organ {
+public:
+	Organ(float agingRate, float health) : agingRate(agingRate), health(health) {}
+
+	float getAgingRate();
+	float getHealth();
+	void takeDamage(float damage);
+	void regenerateHealth(float health);
+
+private:
+	float agingRate;
+	float health;
+};
