8af64300e24d1ffd64ab20957c356617ebf3ac1d
Kutyl87 <pawel.22088@gmail.com>
2024-05-27 20:29:47 +0000

    Add Unit tests for organism class

---
+++ tests/organismTests.cpp
@@ -0,0 +1,56 @@
+#include <catch2/catch_test_macros.hpp>
+#include "organism.h"
+
+TEST_CASE("Organism: Default constructor", "[Organism]") {
+	Organism organism;
+	REQUIRE(organism.getChildren().empty());
+	REQUIRE(organism.getChildren().empty());
+}
+
+TEST_CASE("Organism: Constructor with parameters", "[Organism]") {
+	std::vector<std::unique_ptr<Object>> children;
+	std::vector<std::unique_ptr<Organ>> organs;
+	std::array<float, 3> position = {0.0f, 0.0f, 0.0f};
+	std::array<float, 3> rotation = {0.0f, 0.0f, 0.0f};
+	std::weak_ptr<Object> parent;
+	children.push_back(std::make_unique<Object>(position, rotation, parent));
+	organs.push_back(std::make_unique<Organ>(0.5, 100));
+	Organism organism(std::move(children), std::move(organs));
+	REQUIRE(organism.getChildren().size() == 1);
+	REQUIRE(organism.getOrgans().size() == 1);
+}
+
+//
+TEST_CASE("Organism: Add child", "[Organism]") {
+	Organism organism;
+	std::array<float, 3> position = {0.0f, 0.0f, 0.0f};
+	std::array<float, 3> rotation = {0.0f, 0.0f, 0.0f};
+	std::weak_ptr<Object> parent;
+	organism.addChild(std::make_unique<Object>(position, rotation, parent));
+	REQUIRE(organism.getChildren().size() == 1);
+}
+
+//
+TEST_CASE("Organism: Add organ", "[Organism]") {
+	Organism organism;
+	organism.addOrgan(std::make_unique<Organ>(0.5, 100));
+	REQUIRE(organism.getOrgans().size() == 1);
+}
+
+//
+TEST_CASE("Organism: Remove child by index", "[Organism]") {
+	Organism organism;
+	std::array<float, 3> position = {0.0f, 0.0f, 0.0f};
+	std::array<float, 3> rotation = {0.0f, 0.0f, 0.0f};
+	std::weak_ptr<Object> parent;
+	organism.addChild(std::make_unique<Object>(position, rotation, parent));
+	organism.removeChildByIndex(0);
+	REQUIRE(organism.getChildren().empty());
+}
+
+TEST_CASE("Organism: Remove organ by index", "[Organism]") {
+	Organism organism;
+	organism.addOrgan(std::make_unique<Organ>(0.5, 100));
+	organism.removeOrganByIndex(0);
+	REQUIRE(organism.getOrgans().empty());
+}
