71ea196bfd3821f81a5c5c30f761acb37a441419
Kutyl87 <pawel.22088@gmail.com>
2024-06-06 23:04:39 +0000

    Migrate to tensors

--- core/include/organism.h
+++ core/include/organism.h
@@ -2,6 +2,8 @@
 
 #include <memory>
 #include <vector>
+#include "Actor.h"
+#include "Critic.h"
 #include "limb.h"
 #include "object.h"
 #include "organ.h"
@@ -21,16 +23,24 @@ public:
 	std::array<float, 8> getJointForces();
 	float getReward(float dirX, float dirZ);
 	std::array<float, 18> getState();
+	void update();
 
 private:
+	std::shared_ptr<Actor> actor;
+	std::shared_ptr<Critic> critic;
 	std::array<float, 8> predict();
 	std::vector<std::unique_ptr<Limb>> children;
 	std::vector<std::unique_ptr<Organ>> organs;
-	std::vector<std::array<float, 8>> actions;
-	std::vector<float> rewards;
+	torch::Tensor actions;
+	torch::Tensor rewards;
 	std::vector<std::array<float, 2>> positions;
-	std::vector<std::array<float, 18>> states;
+	torch::Tensor states;
 	std::array<float, 2> direction;
 	static float generateRandomNumber();
 	static float dotProduct(const std::array<float, 2>& a, const std::array<float, 2>& b);
+	const int maxSteps = 20;
+	int step = 0;
+	std::unique_ptr<torch::optim::Optimizer> critic_optimizer;
+	std::unique_ptr<torch::optim::Optimizer> actor_optimizer;
+	float learning_rate = 0.001;
 };
