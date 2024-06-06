2004a8429be11b216c9c71189fda793095691de4
Kutyl87 <pawel.22088@gmail.com>
2024-06-06 21:55:54 +0000

    Add organism impl update for actor critic approach

--- core/src/organism.cpp
+++ core/src/organism.cpp
@@ -1,6 +1,9 @@
 #include "organism.h"
+#include <random>
 
-Organism::Organism() {
+Organism::Organism(std::array<float, 3> position, std::array<float, 3> rotation, std::weak_ptr<Object> parent,
+				   std::vector<std::unique_ptr<Limb>> children, std::vector<std::unique_ptr<Organ>> organs)
+	: Object(position, rotation, parent) {
 	this->children = std::vector<std::unique_ptr<Limb>>();
 	this->organs = std::vector<std::unique_ptr<Organ>>();
 }
@@ -27,4 +30,57 @@ std::vector<std::unique_ptr<Limb>>& Organism::getChildren() {
 
 std::vector<std::unique_ptr<Organ>>& Organism::getOrgans() {
 	return organs;
+}
+
+float Organism::generateRandomNumber() {
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+	static std::uniform_real_distribution<> dis(-1.0, 1.0);
+	return dis(gen);
+}
+
+std::array<float, 2> Organism::getDirection() const {
+	std::array<float, 2> direction = {generateRandomNumber(), generateRandomNumber()};
+	float magnitude = std::sqrt(direction[0] * direction[0] + direction[1] * direction[1]);
+	direction[0] /= magnitude;
+	direction[1] /= magnitude;
+	return direction;
+}
+
+std::array<float, 8> Organism::getJointForces() {
+	float reward = getReward(direction[0], direction[1]);
+	rewards.push_back(reward);
+	auto state = getState();
+	auto action = predict();
+	states.push_back(state);
+	actions.push_back(action);
+	rewards.push_back(reward);
+
+	return action;
+}
+
+float Organism::getReward(float dirX, float dirZ) {
+	std::array<float, 2> lastPos = positions[positions.size() - 1];
+	std::array<float, 2> currentPos = {getPosition()[0], getPosition()[2]};
+	std::array<float, 2> positionDifference = {currentPos[0] - lastPos[0], currentPos[1] - lastPos[1]};
+	return dotProduct(positionDifference, direction);
+}
+
+float Organism::dotProduct(const std::array<float, 2>& a, const std::array<float, 2>& b) {
+	return a[0] * b[0] + a[1] * b[1];
+}
+
+std::array<float, 18> Organism::getState() {
+	std::array<float, 18> state;
+	for (size_t i = 0; i < 4; i++) {
+		state[2 * i] = children[i]->getRotation()[0];
+		state[2 * i + 1] = children[i]->getChildLimb()->getRotation()[0];
+	}
+	for (size_t i = 0; i < 4; i++) {
+		state[2 * i + 8] = children[i]->getLength();
+		state[2 * i + 9] = children[i]->getChildLimb()->getLength();
+	}
+	state[state.size() - 2] = direction[0];
+	state[state.size() - 1] = direction[1];
+	return state;
 }
