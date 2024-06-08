29ae8f9962adc09a8a78b29642bd4d739eb8a261
Kutyl87 <pawel.22088@gmail.com>
2024-06-08 02:35:00 +0000

    Add util function to find distance between two arrays

--- core/include/utils.h
+++ core/include/utils.h
@@ -1,1 +1,2 @@
 float generateRandomNumber(float min, float max);
+float calculateDistance(const std::array<float, 3>& point1, const std::array<float, 3>& point2);
--- core/src/utils.cpp
+++ core/src/utils.cpp
@@ -1,8 +1,17 @@
+#include <array>
+#include <cmath>
 #include <random>
 
 float generateRandomNumber(float min, float max) {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis(min, max);
-    return dis(gen);
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+	std::uniform_real_distribution<> dis(min, max);
+	return dis(gen);
+}
+
+float calculateDistance(const std::array<float, 3>& point1, const std::array<float, 3>& point2) {
+	float x_diff = point1[0] - point2[0];
+	float y_diff = point1[1] - point2[1];
+	float z_diff = point1[2] - point2[2];
+	return std::sqrt(x_diff * x_diff + y_diff * y_diff + z_diff * z_diff);
 }
